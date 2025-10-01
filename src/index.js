82a56c86a8d5ff846196ca50c777a7c1b1378516
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-10-01 11:16:33 +0300

    Improving Reusablity with Props (Customizing StarRating)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return <div>
+    <StarRating onSetRating={setMovieRating} />
+    <p>This movie was rated {movieRating} stars</p>
+  </div>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode>  
     {/* <App /> */}
 
     <StarRating maxRating={10}/>
+    <StarRating color='red' size={50} className="test" messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]} defaultRating={3}/>
+    <StarRating color='green' size={70} messages={['I', 'II', 'III', "IV", 'V']} />
+
+    <Test />
   </React.StrictMode>
 );
